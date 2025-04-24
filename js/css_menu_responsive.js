d37e22ccfaccfab6e097acd3be896fe4697450d7
sergiogarcia2000 <garciamayosergio@gmail.com>
2025-04-24 19:52:57 +0200

    added folders

---
+++ js/css_menu_responsive.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";//además de ser de la clase topnav, también lo va a ser de responsive
+  } else {
+    x.className = "topnav";//vuelve a ser solo de la clase topnav
+  }
+}
+
+
